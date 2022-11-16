f66b7b05228faa655423c2c74619a7f49539877e
Juri <>
2022-11-16 15:47:47 +0100

    Add get api /correct errors

--- automation/tests/internat.js
+++ automation/tests/internat.js
@@ -5,13 +5,12 @@
 var
     config = require('../config/config_internat.json'),
     async = require('async'),
-    chai = require('chai'),
-    expect = chai.expect,
-    should = chai.should(),
-    request = require('request'),
-    RadiusClient = require('../include/radiusClient.js'),
-    logger = require('../include/logger.js'),
-    log = logger.logger
+        chai = require('chai'),
+        expect = chai.expect,
+        should = chai.should(),
+        request = require('request'),
+        logger = require('../include/logger.js'),
+        log = logger.logger
 
 var getAPIUrl = function(path) {
     return config.api.host;
@@ -26,16 +25,19 @@ chai.Assertion.addProperty('json', function() {
 var ENDPOINT_API_SPORTSMEN = getAPIUrl('/sportsman');
 var ENDPOINT_API_INTERNATIONAL_EVENT = getAPIUrl('intEvent')
 
-var token = null; 
+var token = null;
 
 // Start of testing process
 describe('Internat - automation testing', function(cb) {
 
+//===========================================STRART POST REQUESTS==================================================
+
+
     //Хотим протестировать добавление нового спортсмена в базу данных через апи
     it('should be possible to insert new sportsmen ' + config.internat.sportsmen.name, function(cb) {
         this.timeout(6000);
-        console.log("Starting...");
-        // console.log(ENDPOINT_API_SPORTSMEN)
+        console.log("Starting with POST requests...");
+
         request.post("http://95.217.210.206/api/v1/internat/sportsman", {
             json: true,
             strictSSL: config.api.checkCertificate,
@@ -69,10 +71,9 @@ describe('Internat - automation testing', function(cb) {
         });
     });
 
-     //Хотим протестировать добавление нового международного турнира в базу данных через апи
+    //Хотим протестировать добавление нового международного турнира в базу данных через апи
     it('should be possible to insert new international event ' + config.internat.int_event.name_event, function(cb) {
         this.timeout(6000);
-        console.log("Starting...");
         // console.log(ENDPOINT_API_SPORTSMEN)
         request.post("http://95.217.210.206/api/v1/internat/intEvent", {
             json: true,
@@ -81,12 +82,12 @@ describe('Internat - automation testing', function(cb) {
                 'X-CSession-Token': token
             },
             body: {
-                 year: config.internat.int_event.year,
-                 name_event: config.internat.int_event.name_event,
-                 country: config.internat.int_event.country,
-                 sportsman: config.internat.int_event.sportsman,
-                 weight: config.internat.int_event.weight,
-                 result: config.internat.int_event.result
+                year: config.internat.int_event.year,
+                name_event: config.internat.int_event.name_event,
+                country: config.internat.int_event.country,
+                sportsman: config.internat.int_event.sportsman,
+                weight: config.internat.int_event.weight,
+                result: config.internat.int_event.result
             }
         }, function(err, res, body) {
             try {
@@ -110,10 +111,46 @@ describe('Internat - automation testing', function(cb) {
         });
     });
 
-   //Хотим протестировать добавление нового местного турнира в базу данных через апи
+    //Хотим протестировать добавление нового тренерав базу данныз через апи
+    it('shoud be possible to insert new trainer' + config.internat.trainers.name, function(cb) {
+        this.timeout(6000);
+        // console.log(ENDPOINT_API_TRAINERS)
+        request.post("http://95.217.210.206/api/v1/internat/trainer", {
+            json: true,
+            strictSSL: config.api.checkCertificate,
+            headers: {
+                'X-CSession-Token': token
+            },
+            body: {
+                name: config.internat.trainers.name,
+                surname: config.internat.trainers.surname,
+                phone: config.internat.trainers.phone,
+                category: config.internat.trainers.category
+            }
+        }, function(err, res, body) {
+            try {
+                console.log(body);
+                // console.log(err)
+                // console.log(res)
+                var testCase = "should be possible to insert new trainer - POST request: " + config.internat.trainers.trainer;
+                should.not.exist(err);
+                should.exist(body);
+                res.statusCode.should.equal(200);
+                switch (res.statusCode == 200) {
+                    case true:
+                        log.info("Passed: " + testCase);
+                        break;
+                }
+            } catch (err) {
+                log.error("Failed: " + testCase + '->' + err);
+            }
+            cb();
+        });
+    });
+
+    //Хотим протестировать добавление нового местного турнира в базу данных через апи
     it('should be possible to insert new local event ' + config.internat.local_event.event, function(cb) {
         this.timeout(6000);
-        console.log("Starting...");
         // console.log(ENDPOINT_API_SPORTSMEN)
         request.post("http://95.217.210.206/api/v1/internat/localEvent", {
             json: true,
@@ -122,12 +159,12 @@ describe('Internat - automation testing', function(cb) {
                 'X-CSession-Token': token
             },
             body: {
-                 year: config.internat.local_event.year,
-                 event: config.internat.local_event.event,
-                 city: config.internat.local_event.city,
-                 sportsman: config.internat.local_event.sportsman,
-                 weight: config.internat.local_event.weight,
-                 result: config.internat.local_event.result
+                year: config.internat.local_event.year,
+                event: config.internat.local_event.event,
+                city: config.internat.local_event.city,
+                sportsman: config.internat.local_event.sportsman,
+                weight: config.internat.local_event.weight,
+                result: config.internat.local_event.result
             }
         }, function(err, res, body) {
             try {
@@ -151,12 +188,15 @@ describe('Internat - automation testing', function(cb) {
         });
     });
 
-  //Хотим вытащить все местные турниры с базы данных через апи
+//===========================================END POST REQUESTS==================================================
+
+//===========================================STRART GET REQUESTS==================================================
+    console.log("Starting with GET requests...");
+
+    //Хотим вытащить все местные турниры с базы данных через апи
     it('should be possible to get local events ', function(cb) {
         this.timeout(6000);
-        console.log("Starting...");
-        // console.log(ENDPOINT_API_SPORTSMEN)
-        request.get("http://95.217.210.206/api/v1/internat/localEvent", {
+        request.get("http://95.217.210.206/api/v1/internat/localEvents", {
             json: true,
             strictSSL: config.api.checkCertificate,
             headers: {
@@ -186,36 +226,72 @@ describe('Internat - automation testing', function(cb) {
 
 
 
-/*           
+//===========================================STOP GET REQUESTS=================================================
 
-const SUCCESS = {
-    statuscode: 200,
-    success: true,
-    errorcode: 0
-};
 
-const INVALID_INPUT = {
-    statuscode: 406,
-    success: false,
-    message: "Invalid input"
-};
+    /*           
 
-const CUSTOMER_NOT_FOUND = {
-    statuscode: 404,
-    success: false,
-    message: 'customer not found'
-};
+    const SUCCESS = {
+        statuscode: 200,
+        success: true,
+        errorcode: 0
+    };
 
-const UNEXPECTED_ERROR = {
-    statuscode: 500,
-    success: false,
-    message: 'Unexpected error'
-};
-var token = null;
+    const INVALID_INPUT = {
+        statuscode: 406,
+        success: false,
+        message: "Invalid input"
+    };
 
-it('return data for the given accesspoint (post data) - check attributes and parameters of the spot:  ' + config.prov.hotspotId, function(cb) {
+    const CUSTOMER_NOT_FOUND = {
+        statuscode: 404,
+        success: false,
+        message: 'customer not found'
+    };
+
+    const UNEXPECTED_ERROR = {
+        statuscode: 500,
+        success: false,
+        message: 'Unexpected error'
+    };
+    var token = null;
+
+    it('return data for the given accesspoint (post data) - check attributes and parameters of the spot:  ' + config.prov.hotspotId, function(cb) {
+                this.timeout(6000);
+                request.get(ENDPOINT_API_ACCESSPOINTS_BY_SPOT_ID + "UE-9-30", {
+                    json: true,
+                    strictSSL: config.api.checkCertificate,
+                    headers: {
+                        'X-CSession': token
+                    }
+                }, function(err, res, body) {
+                    try {
+                        console.log(body);
+                        var testCase = "return data for the given accesspoint (post data) - check attributes and parameters of the spot:  " + config.prov.hotspotId;
+                        //chack the parameters
+                        body.spotId.should.equal(config.prov.hotspotId);
+                        res.statusCode.should.equal(200);
+                        switch (res.statusCode == 200 &&
+                            body.spotId == conf.data.ap1.spotId
+
+                        ) {
+                            case true:
+                                log.info("Passed: " + testCase);
+                                break;
+                        }
+                    } catch (err) {
+                        log.error("Failed: " + testCase + ' ->' + err);
+                        should.throw(err);
+                    }
+                    cb();
+                });
+            });
+
+
+
+                    it('should be possible to delete spot: ' + config.prov.hotspotId, function(cb) {
             this.timeout(6000);
-            request.get(ENDPOINT_API_ACCESSPOINTS_BY_SPOT_ID + "UE-9-30", {
+            request.del(ENDPOINT_API_ACCESSPOINTS_BY_SPOT_ID_DELETE + config.prov.hotspotId, {
                 json: true,
                 strictSSL: config.api.checkCertificate,
                 headers: {
@@ -223,15 +299,10 @@ it('return data for the given accesspoint (post data) - check attributes and par
                 }
             }, function(err, res, body) {
                 try {
-                    console.log(body);
-                    var testCase = "return data for the given accesspoint (post data) - check attributes and parameters of the spot:  " + config.prov.hotspotId;
-                    //chack the parameters
-                    body.spotId.should.equal(config.prov.hotspotId);
+                    //console.log(body);
+                    var testCase = "should be possible to delete spot: " + config.prov.hotspotId;
                     res.statusCode.should.equal(200);
-                    switch (res.statusCode == 200 &&
-                        body.spotId == conf.data.ap1.spotId
-
-                    ) {
+                    switch (res.statusCode == 200) {
                         case true:
                             log.info("Passed: " + testCase);
                             break;
@@ -244,39 +315,10 @@ it('return data for the given accesspoint (post data) - check attributes and par
             });
         });
 
+    */
 
 
-                it('should be possible to delete spot: ' + config.prov.hotspotId, function(cb) {
-        this.timeout(6000);
-        request.del(ENDPOINT_API_ACCESSPOINTS_BY_SPOT_ID_DELETE + config.prov.hotspotId, {
-            json: true,
-            strictSSL: config.api.checkCertificate,
-            headers: {
-                'X-CSession': token
-            }
-        }, function(err, res, body) {
-            try {
-                //console.log(body);
-                var testCase = "should be possible to delete spot: " + config.prov.hotspotId;
-                res.statusCode.should.equal(200);
-                switch (res.statusCode == 200) {
-                    case true:
-                        log.info("Passed: " + testCase);
-                        break;
-                }
-            } catch (err) {
-                log.error("Failed: " + testCase + ' ->' + err);
-                should.throw(err);
-            }
-            cb();
-        });
-    });
-
-*/
 
 
- 
-  
 
 });
-
